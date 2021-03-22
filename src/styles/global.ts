c0a618ef2b1384693c999b237080373fb5b439ca
stefannovasky <stefannovasky@gmail.com>
2021-03-22 18:08:09 -0300

    feat: adding css variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,46 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eee;
 
-    html {
-        font-size: 62.5%;
-    }
+    --container: 100rem;
 
-    html, body, #__next {
-        height: 100%;
-    }
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
 
-    body {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  html, body, #__next {
+    height: 100%;
+    background: var(--background);
+    color: var(--white);
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
+  }
 `
 
 export default GlobalStyles
